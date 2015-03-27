e43d7e746ff9832f961dd887dc1ab414b6975d6a
AntSharK <great_ant@hotmail.com>
2015-03-27 04:08:18 -0700

    Multi Layer Sprite
    Added test case for multilayersprite. Basic, INCOMPLETE. A multi-layer
    sprite contains many sprites and draws them in some order, to maintain a
    semblance of 3Dness.

---
+++ MyFirstGame/MyFirstGame.Shared/Sprites/Testing/TestPerson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyFirstGame.Sprites.Testing
+{
+    public class TestPerson : BaseSprite
+    {
+        public float moveDirection = 1;
+
+        public Floor floor;
+
+        public TestPerson(Floor f, Vector2 relativePos):
+            base(ContentLoader.GetTexture("blackball.png"), 
+            new Vector2(f.position.X + relativePos.X, f.position.Y + relativePos.Y))
+        {
+            this.floor = f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            float delta = CurrentGame.GetDelta(gameTime);
+            float newY = this.position.Y + delta * 100 * moveDirection;
+            
+            if (newY > floor.position.Y + floor.texture.Height || newY < floor.position.Y)
+            {
+                this.moveDirection = -this.moveDirection;
+                newY = this.position.Y + delta * 100 * moveDirection;
+            }
+            this.position.Y = newY;
+        }
+    }
+}
